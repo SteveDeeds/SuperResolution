d30ef12249743332c12c20d86711e17c7eb7a338
Matt Deeds <mdeeds@gmail.com>
2021-01-16 22:54:37 -0800

    Update train-matt.

--- train-matt.py
+++ train-matt.py
@@ -3,69 +3,94 @@ import numpy as np
 import cv2
 import glob
 import os
+import random
+
+kInputPadding = 4
+kScaleFactor = 4
+kOutputPatchSize = 256
+
+kInputPatchSize = int(kOutputPatchSize / kScaleFactor)
 
 
 def getModel():
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(
-        1, 13, padding='same',
-        activation='linear', input_shape=(63, 63, 1)))
+    inputShape = (kInputPatchSize, kInputPatchSize, 3)
+    print("Input shape: " + str(inputShape))
+    # See section 6 here:
+    # https://towardsdatascience.com/a-comprehensive-introduction-to-different-types-of-convolutions-in-deep-learning-669281e58215
+    model.add(tf.keras.layers.Conv2DTranspose(
+        3,  # Number of filters
+        13,
+        padding='same',
+        output_padding=kScaleFactor - 1,
+        strides=kScaleFactor,
+        activation='linear',
+        input_shape=inputShape,
+        kernel_initializer='zeros'))
     print("Output shape: " + str(model.output.shape))
+    assert(model.output.shape[1] == kOutputPatchSize)
     model.compile(loss=tf.keras.losses.MeanSquaredError(),
                   optimizer=tf.keras.optimizers.Adam(
-        0.05), metrics=['mean_squared_error', 'accuracy'])
+        0.01), metrics=['mean_squared_error', 'accuracy'])
     model.summary()
     return model
 
 
-def getBlurryImage(image):
+def getSmallImage(image):
     height = image.shape[1]
     width = image.shape[0]
-    small = cv2.resize(image, dsize=(int(height / 4), int(width / 4)))
-    blurry = cv2.resize(small, dsize=(height, width),
-                        interpolation=cv2.INTER_CUBIC)
-    return blurry
+    small = cv2.resize(image, dsize=(
+        int(height / kScaleFactor), int(width / kScaleFactor)))
+    return small
 
 
-def getIOPair(sharp, blurry):
-    width = sharp.shape[0]
-    height = sharp.shape[1]
-    x1 = np.random.randint(0, width - 63)
-    y1 = np.random.randint(0, height - 63)
-    x2 = x1 + 63
-    y2 = y1 + 63
-    sharpPatch = sharp[x1:x2, y1:y2, 0:1]
-    blurryPatch = blurry[x1:x2, y1:y2, 0:1]
-    return (blurryPatch, sharpPatch)
+def getPatch(image, x1, y1, size):
+    x2 = x1 + size
+    y2 = y1 + size
+    return image[x1:x2, y1:y2, 0:3]
 
 
-def getTrainingPatches(sharp, blurry, count):
-    inputPatches = np.empty(shape=(0, 63, 63, 1), dtype='float32')
-    outputPatches = np.empty(shape=(0, 63, 63, 1), dtype='float32')
+def getIOPair(sharp, small):
+    width = sharp.shape[0] / kScaleFactor
+    height = sharp.shape[1] / kScaleFactor
+    x1 = np.random.randint(0, width - kInputPatchSize)
+    y1 = np.random.randint(0, height - kInputPatchSize)
+    sharpPatch = getPatch(sharp,
+                          x1 * kScaleFactor, y1 * kScaleFactor, kOutputPatchSize)
+    smallPatch = getPatch(small, x1, y1, kInputPatchSize)
+    return (smallPatch, sharpPatch)
+
+
+def getTrainingPatches(sharp, small, count):
+    inputPatches = np.empty(
+        shape=(0, kInputPatchSize, kInputPatchSize, 3), dtype='float32')
+    outputPatches = np.empty(
+        shape=(0, kOutputPatchSize, kOutputPatchSize, 3), dtype='float32')
     for index in range(0, count):
-        (i, o) = getIOPair(sharp, blurry)
+        (i, o) = getIOPair(sharp, small)
         inputPatches = np.append(inputPatches, [i], axis=0)
         outputPatches = np.append(outputPatches, [o], axis=0)
     return (inputPatches, outputPatches)
 
 
-def getCoveringPatches(blurry):
-    patches = np.empty(shape=(0, 63, 63, 1), dtype='float32')
-    width = blurry.shape[0]
-    height = blurry.shape[1]
+def getCoveringPatches(small, patchSize):
+    patches = np.empty(
+        shape=(0, patchSize, patchSize, 3), dtype='float32')
+    width = small.shape[0]
+    height = small.shape[1]
     x1 = 0
     while True:
-        x2 = x1 + 63
+        x2 = x1 + patchSize
         y1 = 0
         while True:
-            y2 = y1 + 63
-            blurryPatch = blurry[x1:x2, y1:y2, 0:1]
-            patches = np.append(patches, [blurryPatch], axis=0)
+            y2 = y1 + patchSize
+            smallPatch = small[x1:x2, y1:y2, 0:3]
+            patches = np.append(patches, [smallPatch], axis=0)
             y1 = y2
-            if (y1 + 63 > height):
+            if (y1 + patchSize > height):
                 break
         x1 = x2
-        if (x1 + 63 > width):
+        if (x1 + patchSize > width):
             break
     return patches
 
@@ -90,12 +115,17 @@ def trainModel(model, inputs, outputs):
 def evalModel(model, inputs, truths):
     print("Inputs shape: " + str(inputs.shape))
     print("First shape: " + str(np.array(inputs[0]).shape))
-    for index in range(inputs.shape[0]):
+
+    indexes = random.sample(range(0, inputs.shape[0]), 5)
+    for index in indexes:
         outputs = model.apply(inputs)
 
         outputFile = os.path.join("eval", "comaprison-"+str(index)+".png")
         print("Output file: " + outputFile)
-        inputImg = inputs[index, :, :, :] * 255
+        inputImg = cv2.resize(
+            inputs[index, :, :, :] * 255,
+            dsize=(kOutputPatchSize, kOutputPatchSize),
+            interpolation=cv2.INTER_NEAREST)
         outputImg = np.array(outputs[index, :, :, :]) * 255
         truthImg = truths[index, :, :, :] * 255
         compositeImg = np.append(inputImg, outputImg, axis=1)
@@ -104,23 +134,33 @@ def evalModel(model, inputs, truths):
         cv2.imwrite(outputFile, compositeImg)
 
 
-def main():
-    original = cv2.imread("half-dome.png") / 255
-    blurry = getBlurryImage(original)
-    cv2.imwrite("half-dome-blurry.png", blurry * 255)
-
+def addIOFromImage(filename, inputs, outputs):
+    original = cv2.imread(
+        os.path.join(filename)) / 255
+    small = getSmallImage(original)
     print("Original shape: " + str(original.shape))
-    print("Blurry shape: " + str(blurry.shape))
+    print("Small shape: " + str(small.shape))
 
-    (inputs, outputs) = getTrainingPatches(original, blurry, 1000)
+    (newIns, newOuts) = getTrainingPatches(original, small, 50)
+    return (np.append(inputs, newIns, axis=0),
+            np.append(outputs, newOuts, axis=0))
+
+
+def main():
+    model = getModel()
+
+    inputs = np.empty((0, kInputPatchSize, kInputPatchSize, 3))
+    outputs = np.empty((0, kOutputPatchSize, kOutputPatchSize, 3))
+
+    fileNames = glob.glob(os.path.join("input", "*.jpg"))
+    for fname in fileNames:
+        (inputs, outputs) = addIOFromImage(fname, inputs, outputs)
+
     print("Inputs shape: " + str(inputs.shape))
 
-    model = getModel()
     trainModel(model, inputs, outputs)
-
-    truths = getCoveringPatches(original)
-    covering = getCoveringPatches(blurry)
-    evalModel(model, covering, truths)
+    # TODO: Use different images for evaluation
+    evalModel(model, inputs, outputs)
 
 
 if __name__ == "__main__":
