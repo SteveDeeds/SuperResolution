64ed63040f117862af9df281f8e99209738634a3
SteveDeeds <stevendeeds@yahoo.com>
2021-01-18 08:57:49 -0800

    Update train-matt.py
    added a comment

--- train-matt.py
+++ train-matt.py
@@ -19,7 +19,7 @@ def getModel():
 
 def getBiggerModel():
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=(270,270,3)))
+    model.add(tf.keras.layers.Input(shape=(270,270,3))) #with filter sizes of 9, 3, and 5 the input needs to be 270x270 for a 256x256 output 256 + 9 - 1 + 3 -1 + 5 - 1 = 270
     model.add(tf.keras.layers.Conv2D(128, 9, activation='relu', padding='valid'))
     model.add(tf.keras.layers.Conv2D(64, 3, activation='relu', padding='valid'))
     model.add(tf.keras.layers.Conv2D(3, 5, activation='hard_sigmoid', padding='valid'))
