b798f8becb3babaeb7509a74b5d7f5cdc8f01ad4
Matt Deeds <mdeeds@gmail.com>
2021-01-23 13:37:44 -0800

    Move predict toward assembling result.

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import cv2
 import glob
 import os
@@ -13,13 +13,15 @@ kWriteInputPatches = False
 
 def imageFromPatches(patches, verticalPatchCount):
     resultImage = np.empty(
-        (0, model_def.kInputPatchSize * verticalPatchCount, 3), dtype='float32')
+        (0, model_def.kOutputPatchSize * verticalPatchCount, 3), dtype='float32')
 
     i = 0
     while (i < len(patches)):
         resultLine = np.empty(
-            (model_def.kInputPatchSize, 0, 3), dtype='float32')
+            (model_def.kOutputPatchSize, 0, 3), dtype='float32')
         for y in range(0, verticalPatchCount):
+            if i >= len(patches):
+                break
             resultLine = np.append(resultLine, patches[i], axis=1)
             i += 1
         resultImage = np.append(resultImage, resultLine, axis=0)
@@ -41,24 +43,16 @@ def main():
                 os.path.join("eval", "input-patch" + str(i) + ".png"),
                 patch * 255.0)
             i = i + 1
-    # Loading the model fails for me, so I'm commenting this out
-    # model = load_model("check_10.hdf5",
-    #                    custom_objects={'my_psnr': model_def.my_psnr})
-    # results = model.predict(patches)
-    # # output = imageFromPatches(results)
-    # i = 0
-    # for patch in results:
-    #     cv2.imwrite(
-    #         os.path.join("eval", "output-patch" + str(i) + ".png"),
-    #         patch * 255.0)
-    #     i = i + 1
+    model = load_model("check_10.hdf5",
+                       custom_objects={'my_psnr': model_def.my_psnr})
+    results = model.predict(patches)
     verticalPatchCount = int(
         blurry.shape[1]  # Should be img.shape[1]
-        / (model_def.kInputPatchSize - 2 * model_def.kInputPadding))
+        / (model_def.kOutputPatchSize - 2 * model_def.kInputPadding))
 
     # Because model loading failed, I'm using 'patches' here but
     # it really should be 'results'.
-    assembled = imageFromPatches(patches, verticalPatchCount)
+    assembled = imageFromPatches(results, verticalPatchCount)
     print("Final shape: " + str(assembled.shape))
     cv2.imwrite(os.path.join("eval", "assembled.png"), assembled * 255.0)
 
